ad554bb28effcfb37cedd5d358f5952af3dbca5d
Shidou <triada.mzque@gmail.com>
2024-06-28 18:29:32 +0300

    greks

--- src/App.js
+++ src/App.js
@@ -1,25 +1,66 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import Items from "./components/Items";
 
-function App() {
+class App extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      orders: [],
+      items: [
+        {
+          id: 1,
+          title: 'Balenciaga 10xl sneaker',
+          img: '10xl.jpg',
+          desc: '10XL Sneaker in grey and yellow mesh, TPU and rubber is from look 10 of Balenciaga Fall 24 Collection.',
+          category: 'Sneakers',
+          price: '1000$'
+        },
+        {
+          id: 2,
+          title: 'DOUBLE SLEEVE BOMBER',
+          img: 'bomber_balenci.jpg',
+          desc: 'Double Sleeve Bomber in black nylon',
+          category: 'Jackets',
+          price: '100$'
+        },
+        {
+          id: 3,
+          title: 'Off-White Tie Dye Contour Cargo Pant',
+          img: 'offwhite_pants.jpg',
+          desc: 'teal blue cotton tie-dye pattern mid-rise straight leg multiple cargo pockets belt loops front button',
+          category: 'Pants',
+          price: '300$'
+        }
+      ]
+    }
+    this.addToOrder = this.addToOrder.bind(this)
+    this.deleteToOrder = this.deleteOrder.bind(this)
+  }
+  render() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+      <div className="wrapper">
+        <Header orders={this.state.orders} onDelete={this.deleteOrder} />
+        <Items items={this.state.items} onAdd={this.addToOrder} />
+        <Footer />
+      </div>
+    )
+  }
+
+  deleteOrder(id) {
+    this.setState({ orders: this.state.orders.filter(el => el.id !== id ) })
+  }
+
+  addToOrder(item) {
+    let isInArray = false
+    this.state.orders.forEach(el => {
+      if(el.id === item.id)
+        isInArray = true
+    })
+    if(!isInArray)
+    this.setState({orders: [...this.state.orders, item] })
+  }
 }
 
 export default App;
