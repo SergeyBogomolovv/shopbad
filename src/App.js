8f11f31e3103c855e2f840d9e361555c23091454
Sergey Bogomolov <bogomolovs693@gmail.com>
2024-06-28 18:42:41 +0300

    fixed

--- src/App.js
+++ src/App.js
@@ -5,61 +5,60 @@ import Items from "./components/Items";
 
 class App extends React.Component {
   constructor(props) {
-    super(props)
+    super(props);
     this.state = {
       orders: [],
       items: [
         {
           id: 1,
-          title: 'Balenciaga 10xl sneaker',
-          img: '10xl.jpg',
-          desc: '10XL Sneaker in grey and yellow mesh, TPU and rubber is from look 10 of Balenciaga Fall 24 Collection.',
-          category: 'Sneakers',
-          price: '1000$'
+          title: "Balenciaga 10xl sneaker",
+          img: "10xl.jpg",
+          desc: "10XL Sneaker in grey and yellow mesh, TPU and rubber is from look 10 of Balenciaga Fall 24 Collection.",
+          category: "Sneakers",
+          price: "1000$",
         },
         {
           id: 2,
-          title: 'DOUBLE SLEEVE BOMBER',
-          img: 'bomber_balenci.jpg',
-          desc: 'Double Sleeve Bomber in black nylon',
-          category: 'Jackets',
-          price: '100$'
+          title: "DOUBLE SLEEVE BOMBER",
+          img: "bomber_balenci.jpg",
+          desc: "Double Sleeve Bomber in black nylon",
+          category: "Jackets",
+          price: "100$",
         },
         {
           id: 3,
-          title: 'Off-White Tie Dye Contour Cargo Pant',
-          img: 'offwhite_pants.jpg',
-          desc: 'teal blue cotton tie-dye pattern mid-rise straight leg multiple cargo pockets belt loops front button',
-          category: 'Pants',
-          price: '300$'
-        }
-      ]
-    }
-    this.addToOrder = this.addToOrder.bind(this)
-    this.deleteToOrder = this.deleteOrder.bind(this)
+          title: "Off-White Tie Dye Contour Cargo Pant",
+          img: "offwhite_pants.jpg",
+          desc: "teal blue cotton tie-dye pattern mid-rise straight leg multiple cargo pockets belt loops front button",
+          category: "Pants",
+          price: "300$",
+        },
+      ],
+    };
+    this.addToOrder = this.addToOrder.bind(this);
+    this.deleteOrder = this.deleteOrder.bind(this);
   }
   render() {
-  return (
+    return (
       <div className="wrapper">
         <Header orders={this.state.orders} onDelete={this.deleteOrder} />
         <Items items={this.state.items} onAdd={this.addToOrder} />
         <Footer />
       </div>
-    )
+    );
   }
 
   deleteOrder(id) {
-    this.setState({ orders: this.state.orders.filter(el => el.id !== id ) })
+    this.setState({ orders: this.state.orders.filter((el) => el.id !== id) });
   }
 
   addToOrder(item) {
-    let isInArray = false
-    this.state.orders.forEach(el => {
-      if(el.id === item.id)
-        isInArray = true
-    })
-    if(!isInArray)
-    this.setState({orders: [...this.state.orders, item] })
+    let isInArray = false;
+    this.state.orders.forEach((el) => {
+      if (el.id === item.id) isInArray = true;
+    });
+    console.log(this.state.orders);
+    if (!isInArray) this.setState({ orders: [...this.state.orders, item] });
   }
 }
 
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,42 +1,50 @@
-import React, { useState } from 'react'
+import React, { useState } from "react";
 import { FaShoppingBag } from "react-icons/fa";
-import Order from './Order';
+import Order from "./Order";
 
 const showOrders = (props) => {
-  return (<div>
-    {props.orders.map(el => (
-                    <Order onDelete={props.onDelete} key={el.id} item={el} />
-                  ))}
-  </div>)
-}
+  return (
+    <div>
+      {props?.orders &&
+        props?.orders.map((el) => (
+          <Order onDelete={props.onDelete} key={el.id} item={el} />
+        ))}
+    </div>
+  );
+};
 
 const showNothing = () => {
-  return(<div className='empty'>
-    <h2>Add Products</h2>
-  </div>)
-}
+  return (
+    <div className="empty">
+      <h2>Add Products</h2>
+    </div>
+  );
+};
 
 export default function Header(props) {
   let [cartOpen, setCartOpen] = useState(false);
 
   return (
     <header>
-        <div>
-            <span className='logo'>RModel</span>
-            <ul className='nav'>
-                <li>About us</li>
-                <li>Contacts</li>
-                <li>Adress</li>
-            </ul>
-            <FaShoppingBag onClick={() => setCartOpen(cartOpen = !cartOpen)} className={`shop-cart-button ${cartOpen && 'active'}`} />
+      <div>
+        <span className="logo">RModel</span>
+        <ul className="nav">
+          <li>About us</li>
+          <li>Contacts</li>
+          <li>Adress</li>
+        </ul>
+        <FaShoppingBag
+          onClick={() => setCartOpen((cartOpen = !cartOpen))}
+          className={`shop-cart-button ${cartOpen && "active"}`}
+        />
 
-              {cartOpen && (
-                <div className='shop-cart'>
-                  {props.orders.length > 0 ? showOrders(props) : showNothing()}
-                </div>
-              )}
-        </div>
-        <div className='presentation'></div>
+        {cartOpen && (
+          <div className="shop-cart">
+            {props.orders.length > 0 ? showOrders(props) : showNothing()}
+          </div>
+        )}
+      </div>
+      <div className="presentation"></div>
     </header>
-  )
+  );
 }
